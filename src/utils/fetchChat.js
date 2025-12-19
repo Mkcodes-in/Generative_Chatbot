feb532053e64625cb741d2c7a7563a461128608f
Munna -Dev <mkcodes.in@gmail.com>
2025-12-19 10:53:29 +0530

    authentication complete with bugs

--- src/utils/fetchChat.js
+++ src/utils/fetchChat.js
@@ -1,17 +1,19 @@
 import { supabase } from "../supabase/supabase";
 
 export async function fetchChat() {
-     const {data: { user },} = await supabase.auth.getUser();
+  const { data: { user } } = await supabase.auth.getUser();
 
-    try {
-        const data = await supabase
-            .from("messages")
-            .select("chat_id, message, role, created_at")
-            .eq("user_id", user?.id)
-            .order("created_at", { ascending: false });
-        // console.log(data)
-        return data;
-    } catch (error) {
-        console.error("Error : ", error);
-    }
+  if(!user) return;
+
+  try {
+    const data = await supabase
+      .from("messages")
+      .select("chat_id, message, role, created_at")
+      .eq("user_id", user?.id)
+      .order("created_at", { ascending: false });
+    // console.log(data)
+    return data;
+  } catch (error) {
+    console.error("Error : ", error);
+  }
 }
